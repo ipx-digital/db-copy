8ce437ea608beb887dea7ce0f6d08efc8e06fa4f
davisgibson <vagrant@homestead>
2020-03-25 20:38:47 +0000

    It's now working!

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# db-copy
+# DB Copy
+Quickly and easily refresh your local MySQL database with production data by automating the process of copying your production database to your local development database.
+
+## Installation
+1. Use `composer require ipx-digital/db-copy` in your project's directory to include it.
+2. In composer.json, add this line to "autoload-dev" -> "psr-4":
+	```
+	"autoload-dev": {
+	    "psr-4": {
+	        w"IPX\\": "packages/db-copy/src"
+	    }
+	}
+	```
+3. Use `composer dump-autoload`.
+4. In config/app.php, under 'providers' add:
+	`IPX\DbCopy\DbCopyServiceProvider::class`
+5. Add the following to .env:
+	```
+	PRODUCTION_DB_CONNECTION=mysql
+	PRODUCTION_DB_HOST=<address to production DB>
+	PRODUCTION_DB_DATABASE=<database name>
+	PRODUCTION_DB_USERNAME=<mysql username>
+	PRODUCTION_DB_PASSWORD=<mysql password>
+	SSH_USERNAME=<only needed if you're connecting over SSH>
+	```
+## Usage
+Simply use `php artisan db:copy`.
+
+---
+#### Disclaimer
+We're dealing with databases here, so remember to be careful. Make backups often and be careful with protected data and PII concerns.
+
+---
+#### Built by IPX (link) for internal use with largely Laravel projects, but works with any PHP and MySQL application or framework.
--- src/Console/CopyCommand.php
+++ src/Console/CopyCommand.php
@@ -45,6 +45,7 @@ class CopyCommand extends Command
         $dump = $this->dumpProductionDB();
         $this->wipeDevelopmentDB();
         $this->importDumpToDevelopmentDB($dump);
+        $this->info("Done.");
     }   
 
     public function confirmWipe()
@@ -64,9 +65,20 @@ class CopyCommand extends Command
         $password = env('PRODUCTION_DB_PASSWORD');
         $database = env('PRODUCTION_DB_DATABASE');
         $host = env('PRODUCTION_DB_HOST');
-        $options = "--host {$host} -u {$username} -p{$password} {$database}";
+        $ssh_username = env('SSH_USERNAME');
         $dumpDir = "packages/db-copy/dump.sql";
-        shell_exec("mysqldump {$options} > {$dumpDir}");
+        $options = "-u {$username} -p{$password} {$database}";
+        if($ssh_username)
+        {
+            $this->info("Creating SQL dump from {$ssh_username}@{$host} over SSH...");
+            exec("ssh {$ssh_username}@{$host} 'mysqldump {$options}' > {$dumpDir} 2> /dev/null");
+        }
+        else
+        {
+            $this->info("Creating SQL dump from {$host}...");
+            exec("mysqldump -h {$host} {$options} > {$dumpDir} 2> /dev/null");
+        }
+        $this->info("SQL dump created.");
         return $dumpDir;
     }
 
@@ -77,16 +89,15 @@ class CopyCommand extends Command
         $database = env('DB_DATABASE');
         $host = env('DB_HOST');
 
-        exec("mysql -u {$username} -p{$password} -h {$host} -e'show tables;' {$database}", $tables);
-        exec("mysql -u {$username} -p{$password} -h {$host} -e'SET FOREIGN_KEY_CHECKS = 0;' {$database}");
-
+        exec("mysql -u {$username} -p{$password} -h {$host} -e'show tables;' {$database} 2> /dev/null", $tables);
         array_shift($tables);
+
+        $this->info("Wiping development database...");
 
         foreach($tables as $table)
         {
-            exec("mysql -u {$username} -p{$password} -h {$host} -e'DROP TABLE IF EXISTS {$table};' {$database}");
+            exec("mysql -u {$username} -p{$password} -h {$host} -e'SET FOREIGN_KEY_CHECKS = 0;DROP TABLE IF EXISTS {$table};SET FOREIGN_KEY_CHECKS = 1;' {$database} 2> /dev/null");
         }
-        exec("mysql -u {$username} -p{$password} -h {$host} -e'SET FOREIGN_KEY_CHECKS = 1;' {$database}");
     }
 
     public function importDumpToDevelopmentDB($dump)
@@ -95,6 +106,8 @@ class CopyCommand extends Command
         $password = env('DB_PASSWORD');
         $database = env('DB_DATABASE');
         $host = env('DB_HOST');
-        shell_exec("mysql -u {$username} -p{$password} {$database} < {$dump}");
+        $this->info("Importing dump into development database...");
+        exec("mysql -u {$username} -p{$password} {$database} < {$dump} 2> /dev/null");
+        exec("rm " . $dump);
     }
 }
